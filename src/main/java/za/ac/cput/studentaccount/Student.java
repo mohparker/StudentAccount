2600dfc551f270024c6d02e2923dd6a742b49fd6
27630 <27630@LAPTOP-QQSQDDOI>
2021-03-31 04:44:22 +0200

    Commiting the project

---
+++ src/main/java/za/ac/cput/studentaccount/Student.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package za.ac.cput.studentaccount;
+
+/**
+ *
+ * @author 27630
+ */
+public class Student {
+    
+}
