b4497f6ba27c21ef5e44b443a394320d62ddf84d
Chuma Nxazonke - 219181187 <80419887+Chuma-219181187@users.noreply.github.com>
2021-03-31 11:15:13 +0200

    Updated version of StudentTest.java
    This is an updated version of student test class from my project.

--- src/test/java/za/ac/cput/studentaccount/StudentTest.java
+++ src/test/java/za/ac/cput/studentaccount/StudentTest.java
@@ -16,31 +16,182 @@ import static org.junit.Assert.*;
  *
  * @author 27630
  */
-public class StudentTest {
+public class StudentTest
+{
+     private Student student;
+     private StudentTest studentaccount1;
+     private StudentTest studentaccount2;
+     private StudentTest studentaccount3;
     
-    public StudentTest() {
-    }
     
     @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
+    public static void setUpClass() throws Exception
+    {
+        
     }
 
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+  
+    @Before
+    public void setUp()
+    {
+        studentaccount1 = new StudentTest();
+        studentaccount2 = new StudentTest();
+        studentaccount3 = studentaccount1;
+    }
+
+    @After
+    public void tearDown() throws Exception
+    {
+        
+    }
+        @Test
+        public void testEquality (){ //Testing for equality between two instances
+            assertEquals(studentaccount1, studentaccount3);
+   
+        }
+        
+         @Test
+        public void testIdentity (){ //Testing for object identity between two instances
+          assertSame(studentaccount1, studentaccount3);
+            
+        }
+       
+         @Test(enabled = false)
+          public void testDisplayStudentName1() { //This method if the method going tpo fail or not when entering the wrong string name
+            this.student = new Student();
+            
+            String studentName = student.displayStudentName1("Chuma", "Nxazonke");
+            assertEquals("ChumaNxazonke", studentName);
+  
+        }
+        
+        @Test(timeout = 300)
+	        public void testwithTmeout() throws InterruptedException { //This method will pass if the execution of the test method finishes within the time-out 300
+		    Thread.sleep(200);
+		    System.out.println("Timeout method executed");
+	}
+     
+    /**
+     * Test of getName method, of class Student.
+     */
     @Test
-    public void testSomeMethod() {
+   @Ignore
+    public void testGetName() {
+        System.out.println("getName");
+        student.setName("Chuma");
+        String expResult = "";
+        String result = student.getName();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+       
+    }
+
+    /**
+     * Test of setName method, of class Student.
+     */
+    @Test
+    @Ignore
+    public void testSetName() {
+        System.out.println("setName");
+        String name = "";
+        Student instance = new Student();
+        instance.setName(name);
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
     }
+
+    /**
+     * Test of getSurname method, of class Student.
+     */
+    @Test
+    @Ignore
+    public void testGetSurname() {
+        System.out.println("getSurname");
+        Student instance = new Student();
+        String expResult = "";
+        String result = instance.getSurname();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setSurname method, of class Student.
+     */
+    @Test
+    @Ignore
+    public void testSetSurname() {
+        System.out.println("setSurname");
+        String surname = "";
+        Student instance = new Student();
+        instance.setSurname(surname);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getDepartment method, of class Student.
+     */
+    @Test
+    @Ignore
+    public void testGetDepartment() {
+        System.out.println("getDepartment");
+        Student instance = new Student();
+        String expResult = "";
+        String result = instance.getDepartment();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setDepartment method, of class Student.
+     */
+    @Test
+    @Ignore
+    public void testSetDepartment() {
+        System.out.println("setDepartment");
+        String department = "";
+        Student instance = new Student();
+        instance.setDepartment(department);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of toString method, of class Student.
+     */
+    @Test
+    @Ignore
+    public void testToString() {
+        System.out.println("toString");
+        Student instance = new Student();
+        String expResult = "";
+        String result = instance.toString();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of displayStudentName method, of class Student.
+     */
+    @Test
+    @Ignore
+    public void testDisplayStudentName() {
+        System.out.println("displayStudentName");
+        String a = "";
+        String b = "";
+        Student instance = new Student();
+        String expResult = "";
+        String result = instance.displayStudentName1(a, b);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+        
+    }  
     
 }
